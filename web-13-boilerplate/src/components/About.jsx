73a31419700d4a6d476b0a866a6e6a85a913bc88
Aaradhana Shukla <67469200+Aaradhana29@users.noreply.github.com>
2022-03-16 18:17:38 +0530

    Delete web-13-boilerplate directory

--- web-13-boilerplate/src/components/About.jsx
+++
@@ -1,8 +0,0 @@
-export const About = () => {
-
-
-    // make a request to get the details
-  return <>
-    <h2>About</h2>
-  </>;
-};
--- web-13-boilerplate/src/components/Home.jsx
+++
@@ -1,8 +0,0 @@
-export const Home = () => {
-
-
-    // make a request to get the details
-  return (
-   <h2>Home</h2>
-  );
-};
